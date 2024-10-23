cb30c1b27ac62db87df169814425b6bf7421eee2
Valerii Bubenshchikov <valerii_bubenshchikov@epam.com>
2024-10-24 00:38:21 +0400

    augustTV

--- apps/web/components/video-thumbnail.tsx
+++ apps/web/components/video-thumbnail.tsx
@@ -1,5 +1,7 @@
+import { AuthorPicture } from '@/components/AuthorPicture';
 import { ThumbnailPicture } from '@/components/thumbnail-picture';
 
+import moment from 'moment';
 import Link from 'next/link';
 
 export const VideoThumbnail = ({
@@ -35,6 +37,18 @@ export const VideoThumbnail = ({
         width={600}
         height={600}
       />
+      <div className='flex gap-2'>
+        <AuthorPicture author={video.author} />
+        <div>
+          <h3 className='text-lg font-medium max-h-12 leading-6 overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-box-orient:vertical] [-webkit-line-clamp:2] '>
+            {video.title}
+          </h3>
+          <p className='text-sm'>{video.author.nickname}</p>
+          <p className='text-sm'>
+            {moment(video.createdAt).format('MMMM Do, YYYY')}
+          </p>
+        </div>
+      </div>
     </Link>
   );
 };
